8f7c5534da06886f47387a5724e24d7b177f1da9
SkIym <abramjosh13@gmail.com>
2023-07-17 13:46:16 +0800

    Add simple UI

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function getComputerChoice() {
 
     // Return a string that corresponds to the array index
     return choices[computerSelection];
-}
+};
 
 // Create a function to get player's input
 function getPlayerChoice() {
@@ -18,7 +18,7 @@ function getPlayerChoice() {
     let playerSelection = prompt("Type your choice:", "Rock, Paper, or Scissors?");
     return playerSelection.toLowerCase();
 
-}
+};
 
 // Play a single round of the game
 function playGame(player, comp) {
@@ -26,25 +26,18 @@ function playGame(player, comp) {
     // Compare player and computer selections
     if ((player == "rock" && comp == "scissors") || (player == "paper" && comp == "rock") || (player =="scissors" && comp == "paper")) {
         playerPoints += 1;
-        return `You win, ${player} beats ${comp}`;
+        return `You win, ${player} beats ${comp}.`;
     }
     else if (player == comp) {
         return "Draw";
     }
     else {
         computerPoints += 1;
-        return `You lose, ${comp} beats ${player}`;
+        return `You lose, ${comp} beats ${player}.`;
     }
         
-}
+};
 
-// Play the game n amount of times
-function game(n) {
-    for (let i=0; i < n; i++) {
-        console.log(playGame(getPlayerChoice(),getComputerChoice()))
-    }
-    console.log(winner(playerPoints, computerPoints))
-}
 
 // Determine the winner
 function winner(player, comp) {
@@ -53,8 +46,29 @@ function winner(player, comp) {
     else return "Winner: Computer"
 }
 
+// Initialize variables
 let playerPoints = 0;
 let computerPoints = 0;
 
-// Calls the game function with a prompt as the argument
-game(Number(prompt("Number of Rounds:")))
+const selection = document.querySelectorAll('button');
+let player = document.querySelector(".player-points")
+let computer = document.querySelector(".comp-points")
+let recap = document.querySelector(".recap")
+
+// Add listeners for each player pick
+selection.forEach((btn) => {
+
+    btn.addEventListener('click', () => {
+
+        recap.textContent = playGame(btn.value, getComputerChoice())
+        player.textContent = playerPoints;
+        computer.textContent = computerPoints;
+        if (playerPoints == 5 || computerPoints == 5){
+            recap.textContent = winner(playerPoints, computerPoints);
+            playerPoints = 0;
+            computerPoints = 0;
+        }
+    });
+});
+
+
